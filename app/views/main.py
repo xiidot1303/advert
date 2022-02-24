adaf6ce3994303594006b3b031f780bc9b75be07
xiidot1303 <xii1303@inbox.ru>
2022-02-24 20:09:16 +0500

    chart posts and users

--- app/views/main.py
+++ app/views/main.py
@@ -3,7 +3,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from datetime import datetime
+from django.db.models.functions import TruncDay
+from django.db.models import Count
+
+from datetime import datetime, timedelta
 from app.models import *
 
 @login_required
@@ -15,12 +18,24 @@ def main_menu(request):
     count_bot_users = len(Bot_user.objects.all())
     today = datetime.now()
     count_daily_bot_users = len(Bot_user.objects.filter(date__year=today.year, date__month=today.month, date__day=today.day))
-    bot_users = [count_bot_users, count_daily_bot_users]
+    amount_bot_users = [count_bot_users, count_daily_bot_users]
 
-    bot_clients = len(Bot_user.objects.filter(is_client=True))
+    amount_bot_clients = len(Bot_user.objects.filter(is_client=True))
 
-    context = {'bot_users': bot_users, 'bot_clients': bot_clients}
-    return render(request, 'views/main.html')
+
+    def make_date(obj):
+        obj = obj + timedelta(hours=5)
+        return obj.strftime("%Y-%m-%dT%H:%m:%S")
+        
+    dates_users = list(map(lambda l: make_date(l[0]), list(Bot_user.objects.annotate(day=TruncDay('date')).values('day').annotate(c=Count('id')).values_list('day', 'c'))))
+    dates_posts = list(map(lambda l: make_date(l[0]), list(Statement.objects.filter(status='confirmed').annotate(answer__day=TruncDay('answer__date')).values('answer__day').annotate(c=Count('id')).values_list('answer__day', 'c'))))
+
+    bot_users_by_date = list(map(lambda l: int(l[1]), list(Bot_user.objects.annotate(day=TruncDay('date')).values('day').annotate(c=Count('id')).values_list('day', 'c'))))
+    
+    posts_by_date = list(map(lambda l: int(l[1]), list(Statement.objects.filter(status='confirmed').annotate(answer__day=TruncDay('answer__date')).values('answer__day').annotate(c=Count('id')).values_list('answer__day', 'c'))))
+
+    context = {'amount_bot_users': amount_bot_users, 'amount_bot_clients': amount_bot_clients, 'bot_users_by_date': bot_users_by_date, 'dates_users': dates_users, 'posts_by_date': posts_by_date, 'dates_posts': dates_posts}
+    return render(request, 'views/main.html', context)
 
 @login_required
 def get_photos(request, folder, file):
